2717718f9ec009339d7e95fc917339b90efd3be7
Anton Ivlev <anton.ivlev97@gmail.com>
2018-01-20 16:37:04 +0000

    labeling and panning work, hold alt

--- client/src/LabelOptions.js
+++ client/src/LabelOptions.js
@@ -13,9 +13,8 @@ class LabelOptions extends React.Component {
     super(props);
     this.state = {
       timesList: [
-        [1481717805329.4575, 1481717806192.528]
+        ['blue', 1481717805329.4575, 1481717806192.528]
       ],
-      colourList: [],
 
       selectedLabel: null,
       selectedColour: 0
@@ -26,13 +25,15 @@ class LabelOptions extends React.Component {
     document.addEventListener('keydown', (e) => {
       if (e.altKey) {
         document.getElementById('svg-labels').style.pointerEvents = "all";
-      } else if (e.code === 'Delete') {
-        let tlist = this.state.timesList;
-        if (this.state.selectedLabel !== null) {
-          tlist.splice(this.state.selectedLabel, 1);
-          this.setState( {'timesList': tlist} );
+        if (e.code === 'Delete') {
+          let tlist = this.state.timesList;
+          if (this.state.selectedLabel !== null) {
+            tlist.splice(this.state.selectedLabel, 1);
+            this.setState( {'timesList': tlist} );
+          }
         }
-      } else if (e.code.includes('Digit')) {
+      }
+      if (e.code.includes('Digit')) {
         this.setState( {'selectedColour': parseInt(e.key)} );
       }
     });
@@ -43,70 +44,61 @@ class LabelOptions extends React.Component {
 
     // attach drag listener for drawing
     var that = this;
-    // d3.select('#svg-labels').call(d3.drag()
-    //   .on('start', function() {
-    //     if (d3.event.sourceEvent.altKey) {
-    //       let timesList = that.state.timesList;
-    //       let pixelToTime = that.props.timeToPixel.invert;
-    //
-    //       let startp = pixelToTime(d3.event.sourceEvent.screenX);
-    //       timesList.push( [startp, startp+1] );
-    //       that.setState( {'timesList': timesList} );
-    //     }
-    //   })
-    //   .on('drag', function() {
-    //     if (d3.event.sourceEvent.altKey) {
-    //       let timesList = that.state.timesList;
-    //       let pixelToTime = that.props.timeToPixel.invert;
-    //
-    //       last(timesList)[1] = pixelToTime(d3.event.sourceEvent.screenX);
-    //       that.setState( {'timesList': timesList} );
-    //     }
-    //   })
-    // );
+    d3.select('#svg-labels').call(d3.drag()
+      .on('start', function() {
+        if (d3.event.sourceEvent.altKey) {
+          let timesList = that.state.timesList;
+          let pixelToTime = that.props.timeToPixel.invert;
+
+          let colour_choice = [
+            'green', 'blue', 'salmon', 'orange', 'purple',
+            'khaki', 'lightgreen', 'magenta', 'olive', 'brown'
+          ];
+
+          let startp = pixelToTime(d3.event.sourceEvent.screenX);
+          timesList.push( [colour_choice[that.state.selectedColour], startp, startp+1] );
+          that.setState( {'timesList': timesList} );
+        }
+      })
+      .on('drag', function() {
+        if (d3.event.sourceEvent.altKey) {
+          let timesList = that.state.timesList;
+          let pixelToTime = that.props.timeToPixel.invert;
+
+          last(timesList)[2] = pixelToTime(d3.event.sourceEvent.screenX);
+          that.setState( {'timesList': timesList} );
+        }
+      })
+    );
   }
 
   render() {
     var that = this;
-    var colours = [
-      'green', 'blue', 'salmon', 'orange', 'purple',
-      'khaki', 'lightgreen', 'magenta', 'olive', 'brown'
-    ];
 
     let timeToPixel = this.props.timeToPixel;
 
     let annos = this.state.timesList.map( function(region, i) {
-      if (region.length === 2) {
-        let x1 = timeToPixel( region[0] );
+      if (region.length === 3) {
+        let x1 = timeToPixel( region[1] );
         let width = timeToPixel( last(region) ) - x1;
         return (
           <rect key={i}
             className="label-rect"
             x={x1} y="0"
             width={width} height="1200"
-            fill={colours[that.state.selectedColour]}
+            fill={region[0]}
 
             onMouseOver={(e) => {
-              // console.log(e.bubbles);
-              // that.setState( {'selectedLabel': i} );
+              that.setState( {'selectedLabel': i} );
             }}
             onMouseOut={(e) => {
               that.setState( {'selectedLabel': null} );
-            }}
-            onMouseDown={(e) => {
-              console.log(e.bubbles);
             }}
           />
         );
       }
     });
-
-    // implement this but with polylines
-    // let annos = this.state.timesList.map( function(region, i) {
-    //   if (region.length === 2) {
-    //
-    //   }
-    // });
+    console.log(annos);
 
     return (
       <div style={style_obj}>
