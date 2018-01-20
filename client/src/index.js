c8edc859405cbd9f7d818827b5f584f1ca423d96
Anton Ivlev <anton.ivlev97@gmail.com>
2018-01-20 14:45:34 +0000

    simple overlayed svgs and canvas
    tried to figure out how to call all click handlers of layered elements

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import DataDrawer from './DataDrawer.js';
-import LabelOptions from './LabelOptions.js';
-import MyVideo from './MyVideo.js';
-
 import './style.css';
 
 import * as d3 from 'd3';
@@ -14,69 +10,58 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mid_bucket: 0,
-      z: 0
-    };
+      circles: [],
+      squares: []
+    }
+  }
+
+  drawLine(e) {
+    let ctx = document.getElementById('main').getContext('2d');
+    ctx.beginPath();
+    ctx.strokeStyle = 'blue';
+    ctx.moveTo(0, e.nativeEvent.offsetY);
+    ctx.lineTo(500, e.nativeEvent.offsetY);
+    ctx.stroke();
   }
 
   render() {
-    let timeToPixel = getTimeToPixel(
-      this.props.server_store.tvals, this.state.mid_bucket, this.state.z
-    );
-
     return (
       <div>
-        <div>
-          <input type="checkbox" id="video-lock"/>
-          <label htmlFor="video-lock">lock video</label>
-        </div>
+        <canvas id="main"
+          onClick={(e) => this.drawLine(e)}
+        ></canvas>
 
-        <DataDrawer
-          data_list={this.props.server_store.data_list}
-          // so that data drawer updates mid bucket and z for video
-          updateParentState={this.setState.bind(this)}
-          mid_bucket={this.state.mid_bucket}
-          z={this.state.z}
-        />
-        <LabelOptions timeToPixel={timeToPixel} />
-        <MyVideo timeToPixel={timeToPixel} />
+        <svg
+          id="overlay-circ"
+          onClick={ (e) => {
+            let circs = this.state.circles;
+            circs.push(<circle key={e.screenX+e.screenY}
+              cx={e.nativeEvent.offsetX} cy={e.nativeEvent.offsetY} r="10" />)
+            this.setState( {'circles':circs} )
+          } }
+        >
+          {this.state.circles}
+        </svg>
+
+        <svg
+          id="overlay-rect"
+          onClick={ (e) => {
+            console.log('click');
+            let squares = this.state.squares;
+            squares.push(<rect key={e.screenX+e.screenY}
+              x={e.nativeEvent.offsetX} y={e.nativeEvent.offsetY} width="10" height="10" />)
+
+            this.setState( {'squares':squares} )
+          } }
+        >
+          {this.state.squares}
+        </svg>
       </div>
     );
   }
 }
 
-function getTimeToPixel(tvals, mid, z) {
-  let seq = tvals[z];
-  let interval = getInterval(seq);
-  let buck0 = interval[0],
-      buck1 = interval[1];
-
-  var s = d3.scaleLinear()
-    .domain( [d3.mean(seq[buck0]), d3.mean(seq[buck1])] )
-    .range( [400-(mid-buck0), 400-(mid-buck1)] );
-
-  return s;
-}
-
-function getInterval(seq) {
-  let first_ind = 10;
-  while (seq[first_ind] === []) {
-    first_ind++
-  };
-
-  let second_ind = first_ind+100;
-  while (seq[second_ind] === []) {second_ind++};
-
-  return [first_ind, second_ind];
-}
-
-fetch('/seestore')
-  .then( function(res) {
-    return res.json();
-  })
-  .then((server_store) => {
-    ReactDOM.render(
-      <App server_store={server_store} />,
-      document.getElementById('root')
-    );
-  });
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
